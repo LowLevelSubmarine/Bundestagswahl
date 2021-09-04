bd1f4fbb5f2beecde5e28800c909f6a0757fc6d8
Jonbeckas <jonasbeckerx3@outlook.de>
2021-09-05 00:57:31 +0200

    fix: dates are partially fixed; Better performance on many data points

--- frontend/src/app/linear-graph/components/linear-graph/linear-graph.component.ts
+++ frontend/src/app/linear-graph/components/linear-graph/linear-graph.component.ts
@@ -1,16 +1,20 @@
 import {
   AfterViewInit,
   ChangeDetectorRef,
-  Component, ElementRef, HostListener,
+  Component,
+  ElementRef,
+  HostListener,
   Input,
-  Renderer2, ViewChild,
+  Renderer2,
+  ViewChild,
 } from '@angular/core';
-import {ChartElement, ChartElementGroup, YLines} from "../../dto/chartElement.dto";
-import {GroupDto, GroupValueDto} from "../../dto/group-dto";
+import {ChartElement, YLines} from "../../dto/chartElement.dto";
+import {GroupDto} from "../../dto/group-dto";
 import {ViewDimensions} from "./view-dimensions";
 import {InfoBubbleComponent} from "../info-bubble/info-bubble.component";
 import {GradientComponent} from "../gradient/gradient.component";
 import {MarkerDto} from "../../dto/marker.dto";
+
 @Component({
   selector: 'app-linear-graph',
   templateUrl: './linear-graph.component.html',
@@ -23,7 +27,7 @@ export class LinearGraphComponent implements AfterViewInit {
   @ViewChild("svgContainer") svgContainer!: ElementRef
 
   @Input()
-  set data (elements: ChartElement| null) {
+  set data(elements: ChartElement | null) {
     if (elements) {
       this._data = elements
       this.buildGraph(elements)
@@ -31,7 +35,7 @@ export class LinearGraphComponent implements AfterViewInit {
   }
 
   @Input()
-  set highlightedGroup(name: string| undefined) {
+  set highlightedGroup(name: string | undefined) {
     if (name) {
       this.gradientComponent.showGradient(name)
     } else if (this.gradientComponent) {
@@ -39,6 +43,7 @@ export class LinearGraphComponent implements AfterViewInit {
 
     }
   }
+
   @Input()
   minYValue: undefined | number = undefined
   _data!: ChartElement
@@ -49,25 +54,24 @@ export class LinearGraphComponent implements AfterViewInit {
   yMarker: YLines[] = []
   showCircles = true
   bigDataSet = false
+  lastIndex = 0
 
   viewDimensions = new ViewDimensions()
 
-  lastDate: number| undefined = undefined
-  dateWidth = 0
+  constructor(private renderer: Renderer2, private changeDetection: ChangeDetectorRef) {
+  }
 
-  constructor(private renderer: Renderer2, private changeDetection: ChangeDetectorRef) { }
-
-  @HostListener("window:resize",['$event'])
+  @HostListener("window:resize", ['$event'])
   onresize($event: any) {
-      //TODO: fix: this.viewDimensions.svgHeight = this.svgContainer.nativeElement.offsetHeight
-      this.infoBubble.closeBubble()
-      this.viewDimensions.svgWidth = this.svgContainer.nativeElement.offsetWidth
-      this.viewDimensions.recalculateViewDimensions()
-      if (this.viewDimensions.xMultiplier<=290) {
-        this.showCircles = false
-      } else if (!this.bigDataSet) {
-        this.showCircles = true
-      }
+    //TODO: fix: this.viewDimensions.svgHeight = this.svgContainer.nativeElement.offsetHeight
+    this.infoBubble.closeBubble()
+    this.viewDimensions.svgWidth = this.svgContainer.nativeElement.offsetWidth
+    this.viewDimensions.recalculateViewDimensions()
+    if (this.viewDimensions.xMultiplier <= 290) {
+      this.showCircles = false
+    } else if (!this.bigDataSet) {
+      this.showCircles = true
+    }
   }
 
   ngAfterViewInit() {
@@ -75,7 +79,6 @@ export class LinearGraphComponent implements AfterViewInit {
     this.viewDimensions.svgWidth = this.svgContainer.nativeElement.offsetWidth
     this.viewDimensions.recalculateViewDimensions()
   }
-
 
 
   buildGraph(chartElement: ChartElement) {
@@ -91,18 +94,18 @@ export class LinearGraphComponent implements AfterViewInit {
         names.push(element.name)
       }
       for (let serie of element.points) {
-        if (maxValueX == null ||serie.position> maxValueX) {
+        if (maxValueX == null || serie.position > maxValueX) {
           maxValueX = serie.position
         }
 
-        if (maxValueY == null ||serie.y > maxValueY) {
+        if (maxValueY == null || serie.y > maxValueY) {
           maxValueY = serie.y
         }
 
-        if (minValueX == null ||serie.position< minValueX) {
+        if (minValueX == null || serie.position < minValueX) {
           minValueX = serie.position
         }
-        if (minValueY == null ||serie.y< minValueY) {
+        if (minValueY == null || serie.y < minValueY) {
           minValueY = serie.y
         }
 
@@ -119,55 +122,69 @@ export class LinearGraphComponent implements AfterViewInit {
     this.uniPos = []
 
     for (let element of chartElement.chartGroups) {
-      if (element.points.length > this.viewDimensions.xMultiplier) {
-        this.bigDataSet = true
-        this.showCircles = false
-      }
-
-
       for (let serie of element.points) {
         let pos = (serie.position - minValueX) / (maxValueX - minValueX)
         let newY = (serie.y - minValueY) / (maxValueY - minValueY)
 
         if (this.uniPos.filter((it) => it.pos == pos).length == 0) {
-          this.uniPos.push({pos:pos,name:serie.name})
+          this.uniPos.push({pos: pos, name: serie.name})
         }
 
         if (groups.filter((it) => it.group == element.name).length == 0) {
-          groups.push({group:element.name,color: element.color,values:[{position:pos, x: serie.name, y: newY, info: serie.info}]})
+          groups.push({
+            group: element.name,
+            color: element.color,
+            values: [{position: pos, x: serie.name, y: newY, info: serie.info}]
+          })
         } else {
-            groups.filter((it) => it.group == element.name)[0].values.push({position:pos, x: serie.name, y: newY, info: serie.info})
+          groups.filter((it) => it.group == element.name)[0].values.push({
+            position: pos,
+            x: serie.name,
+            y: newY,
+            info: serie.info
+          })
         }
       }
     }
 
+    if (this.uniPos.length > this.viewDimensions.xMultiplier / 2) {
+      this.bigDataSet = true
+      this.showCircles = false
+    }
+
     let yMarker = []
     for (const yLine of chartElement.yLines) {
-      yMarker.push({position:(yLine.position - minValueY) / (maxValueY - minValueY),name:yLine.name, stroke: yLine.stroke})
+      yMarker.push({
+        position: (yLine.position - minValueY) / (maxValueY - minValueY),
+        name: yLine.name,
+        stroke: yLine.stroke
+      })
     }
     this.yMarker = yMarker
 
     this.groups = groups
   }
 
-  getStrokeColor(color:string) {
-    return `stroke:${color};`
+  isShowDate(pos: number, index: number): boolean {
+    const width = this.viewDimensions.getX(0.0425);
+    const lastWidth = this.lastIndex * width
+    const gap = lastWidth + this.viewDimensions.xOffset;
+    const xPos = this.viewDimensions.getX(pos);
+    if (index == 0) {
+      this.lastIndex = 1
+      return true
+    }
+
+    if (xPos >= gap && xPos + width < this.viewDimensions.svgWidth) {
+      this.lastIndex = index+1
+      return true
+    } else {
+      return false
+    }
+
   }
 
-  isShowDate(pos: number): boolean {
-    const margin = 20
-    const lastElements = document.getElementsByClassName("xText")
-     if (lastElements.length == 0) {
-       return true
-     } else {
-       const lastElement = lastElements[lastElements.length-1]
-       const width = lastElements[0].getBoundingClientRect().width;
-       const x = (<any>lastElement).x.baseVal[0].value
-       if (this.viewDimensions.getX(pos) > x+width+margin && this.viewDimensions.getX(pos) +width < this.viewDimensions.svgWidth) {
-         return true
-       } else  {
-         return false
-       }
-     }
+  showPosition(marker: YLines) {
+    return this.viewDimensions.getY(marker.position) > 5
   }
 }
